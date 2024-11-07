1440099b5248be12ba8bd40689c05993054bbe33
skywalker4ce <sky43@byu.edu>
2024-11-06 22:22:42 -0700

    fixed some more directories

--- startup2024/src/login/login.jsx
+++ startup2024/src/login/login.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-import { Unauthenticated } from './unauthenticated';
-import { Authenticated } from './authenticated';
-import { AuthState } from './authState';
+// import { Unauthenticated } from './unauthenticated';
+// import { Authenticated } from './authenticated';
+// import { AuthState } from './authState';
 
 export function Login({ userName, authState, onAuthChange }) {
     return (
