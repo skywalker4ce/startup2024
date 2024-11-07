b7ca02c866d2fcc111c9e749de9edd946c4c1874
skywalker4ce <sky43@byu.edu>
2024-11-06 22:31:19 -0700

    testing

--- startup2024/src/login/login.jsx
+++ startup2024/src/login/login.jsx
@@ -1,28 +1,41 @@
 import React from 'react';
 
+function App() {
+    return (
+        <div>
+            <h1>Welcome to RateIt!</h1>
+        </div>
+    );
+}
+
+export default App;
+
+
+// import React from 'react';
+
 // import { Unauthenticated } from './unauthenticated';
 // import { Authenticated } from './authenticated';
 // import { AuthState } from './authState';
 
-export function Login({ userName, authState, onAuthChange }) {
-    return (
-        <>
-            <div className="text-center mb-5">
-                <p className="lead">Login to create your own rating or view someone else's!</p>
-            </div>
+// export function Login({ userName, authState, onAuthChange }) {
+//     return (
+//         <>
+//             <div className="text-center mb-5">
+//                 <p className="lead">Login to create your own rating or view someone else's!</p>
+//             </div>
 
-            <form method="get" action="index1.html" className="mx-auto" style="max-width: 400px;">
-                <div className="mb-3">
-                    <input type="email" className="form-control" placeholder="your@email.com" required />
-                </div>
-                <div className="mb-3">
-                    <input type="password" className="form-control" placeholder="password" required />
-                </div>
-                <div className="d-flex justify-content-between">
-                    <button type="submit" className="btn btn-primary">Login</button>
-                    <button type="submit" className="btn btn-secondary">Create</button>
-                </div>
-            </form>
-        </>
-    )
-}
+//             <form method="get" action="index1.html" className="mx-auto" style="max-width: 400px;">
+//                 <div className="mb-3">
+//                     <input type="email" className="form-control" placeholder="your@email.com" required />
+//                 </div>
+//                 <div className="mb-3">
+//                     <input type="password" className="form-control" placeholder="password" required />
+//                 </div>
+//                 <div className="d-flex justify-content-between">
+//                     <button type="submit" className="btn btn-primary">Login</button>
+//                     <button type="submit" className="btn btn-secondary">Create</button>
+//                 </div>
+//             </form>
+//         </>
+//     )
+// }
--- startup2024/src/main.jsx
+++ startup2024/src/main.jsx
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+console.log('main.jsx is running');
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
