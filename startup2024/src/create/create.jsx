1a4181080c42c03d9c729e05f313a51fea59b9f0
skywalker4ce <sky43@byu.edu>
2024-11-07 22:31:43 -0700

    fix

--- startup2024/src/create/create.jsx
+++ startup2024/src/create/create.jsx
@@ -19,7 +19,7 @@ export function Create() {
 
         <div className="col-md-3">
           <form onSubmit={handleLogout} className="mx-auto" style={{ maxWidth: '400px' }}>
-            <button type="submit" className="btn btn-primary">Logout</button>
+            <button type="submit" className="btn btn-logout">Logout</button>
           </form>
           <br></br>
           <p className="lead">See another RateIt!<sup>&reg;</sup></p>
