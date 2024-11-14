9a04782688ddf1097469447e958dab40c3f0a16d
skywalker4ce <sky43@byu.edu>
2024-11-13 19:16:13 -0700

    setup

--- startup2024/service/index.js
+++ startup2024/service/index.js
@@ -1,8 +1,13 @@
 const express = require('express');
-const uuid = require('uuid');
 const app = express();
 
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
 app.use(express.static('public'));
 
+// Serve up the front-end static content hosting
+app.use(express.static('public'));
+
+// Router for service endpoints
+var apiRouter = express.Router();
+app.use(`/api`, apiRouter);
