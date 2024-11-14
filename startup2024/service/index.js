136949a6802295574ce755a1dcd97e897f3df189
skywalker4ce <sky43@byu.edu>
2024-11-13 19:17:37 -0700

    updates

--- startup2024/service/index.js
+++ startup2024/service/index.js
@@ -3,11 +3,19 @@ const app = express();
 
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
-app.use(express.static('public'));
-
 // Serve up the front-end static content hosting
 app.use(express.static('public'));
 
 // Router for service endpoints
-var apiRouter = express.Router();
+const apiRouter = express.Router();
 app.use(`/api`, apiRouter);
+
+// Test route to verify server functionality
+app.get('/', (req, res) => {
+  res.send('Server is running!');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
