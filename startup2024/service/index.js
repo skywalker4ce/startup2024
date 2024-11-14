ffa23bbc187122e6cc8fe263a8d6032019c81d07
skywalker4ce <sky43@byu.edu>
2024-11-13 19:04:10 -0700

    setup

--- startup2024/service/index.js
+++ startup2024/service/index.js
@@ -1,3 +1,7 @@
+const express = require('express');
+const uuid = require('uuid');
+const app = express();
+
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
 app.use(express.static('public'));
