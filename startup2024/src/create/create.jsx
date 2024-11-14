e70c83a94c17ec1a38c1eecb1e31c71df939f669
skywalker4ce <sky43@byu.edu>
2024-11-13 20:56:54 -0700

    fix

--- startup2024/src/create/create.jsx
+++ startup2024/src/create/create.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useNavigate } from "react-router-dom";
+import React, { useEffect, useState } from 'react';
 
 export function Create() {
   const [foodItem, setFoodItem] = useState("Category Loading...");
