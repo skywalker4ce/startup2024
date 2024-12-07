97552b38470d6c0e9957397c5601523202216784
skywalker4ce <sky43@byu.edu>
2024-12-07 13:19:11 -0700

    fix?

--- startup2024/service/index.js
+++ startup2024/service/index.js
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public'))); // Serve React static f
 
 // Fallback route to serve index.html for client-side routing
 app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', "index.html"));
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 // Router for service endpoints
