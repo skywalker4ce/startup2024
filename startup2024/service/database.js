6bab1f6bc62ca46b325377db3f68c212fc9c07ff
skywalker4ce <sky43@byu.edu>
2024-12-07 17:04:35 -0700

    websocket complete and ready to deploy

--- startup2024/service/database.js
+++ startup2024/service/database.js
@@ -4,56 +4,44 @@ const uuid = require('uuid');
 const config = require('./dbConfig.json');
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
 
-const client = new MongoClient(url, { tls: true, serverSelectionTimeoutMS: 3000, autoSelectFamily: false });
+const client = new MongoClient(url, { tls: true, serverSelectionTimeoutMS: 3000, autoSelectFamily: false, });
 const db = client.db('startup');
 const userCollection = db.collection('user');
-const ratingCollection = db.collection('rating');
-const categoryCollection = db.collection('category');
+const scoreCollection = db.collection('score');
 
-async function getUser(email) {
-  return await userCollection.findOne({ email });
+// This will asynchronously test the connection and exit the process if it fails
+(async function testConnection() {
+  await client.connect();
+  await db.command({ ping: 1 });
+})().catch((ex) => {
+  console.log(`Unable to connect to database with ${url} because ${ex.message}`);
+  process.exit(1);
+});
+
+function getUser(email) {
+  return userCollection.findOne({ email: email });
 }
 
-async function getUserByToken(token) {
-  return await userCollection.findOne({ token });
+function getUserByToken(token) {
+  return userCollection.findOne({ token: token });
 }
 
 async function createUser(email, password) {
+  // Hash the password before we insert it into the database
   const passwordHash = await bcrypt.hash(password, 10);
-  const user = { email, password: passwordHash, token: uuid.v4() };
+
+  const user = {
+    email: email,
+    password: passwordHash,
+    token: uuid.v4(),
+  };
   await userCollection.insertOne(user);
+
   return user;
 }
 
-async function addRating(userId, category, subCategory, ratingValue) {
-  const rating = {
-    userId,
-    category,
-    subCategory,
-    rating: ratingValue,
-    timestamp: new Date(),
-  };
-  await ratingCollection.insertOne(rating);
-}
-
-async function getRatings(userId) {
-  return await ratingCollection.find({ userId }).toArray();
-}
-
-async function getCategories() {
-  return await categoryCollection.find().toArray();
-}
-
-async function addCategory(category) {
-  await categoryCollection.insertOne({ name: category });
-}
-
 module.exports = {
-  getUser,
-  getUserByToken,
-  createUser,
-  addRating,
-  getRatings,
-  getCategories,
-  addCategory
-};
+    getUser,
+    getUserByToken,
+    createUser
+  };
